627b7404299f7429332ec74c8d420192ebef46c5
marioquiroga <mquiroga@dosalcubo.com>
2021-07-12 22:18:19 -0300

    desafio 8

---
+++ 8. rest/main.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const express = require('express')
+
+const app = express()
+const port = 8001
+
+app.get('/api/productos/listar', (req, res) => {
+  fs.promises.readFile('./productos.txt', 'utf-8')
+    .then(data => {
+      let raw = JSON.parse(data)
+      if(raw.length == 0) raw = {error: 'No hay productos cargados'}
+      res.send(raw)
+    })
+})
+
+app.get('/api/productos/listar/:id', (req, res) => {
+  fs.promises.readFile('./productos.txt', 'utf-8')
+    .then(data => {
+      const raw = JSON.parse(data)
+      let item = raw.filter(prod => prod.id == req.params.id)
+      if(item.length == 0) item = {error: 'Producto no encontrado'}
+      res.send(item)
+    })
+})
+
+app.post('/api/productos/guardar', (req, res) => {
+  fs.promises.readFile('./productos.txt', 'utf-8')
+    .then(data => {
+      let raw = JSON.parse(data)
+      
+      let productoNuevo = {}
+      productoNuevo.title = 'Bicicleta'
+      productoNuevo.price = 1000.50
+      productoNuevo.thumbnail = 'https://cdn3.iconfinder.com/data/icons/education-209/64/bycicle-256.png'
+      productoNuevo.id = raw.slice(-1)[0].id + 1
+
+      let temp = [...raw, productoNuevo]
+      fs.promises.writeFile('./productos.txt', JSON.stringify(temp, null, '\t')) 
+      res.send(productoNuevo)
+    })
+}) 
+
+app.listen(port, () => console.log('listen port: '+port))
